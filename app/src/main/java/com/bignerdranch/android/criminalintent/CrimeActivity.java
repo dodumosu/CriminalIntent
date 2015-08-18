b4c6500abe222cfd5c54c74b800ef04eee5a44e0
Oladipo Odumosu <dodumosu@timbaobjects.com>
2015-08-18 09:56:57 +0100

    Load fragment into activity on startup.

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -1,5 +1,7 @@
 package com.bignerdranch.android.criminalintent;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,5 +13,13 @@ public class CrimeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+
+        if (fragment == null) {
+            fragment = new CrimeFragment();
+            fm.beginTransaction().add(R.id.fragmentContainer, fragment).commit();
+        }
     }
 }
